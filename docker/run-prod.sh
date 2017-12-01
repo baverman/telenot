3a25d1b99ca1b228d9f75d782299f5e5f1278367
Anton Bobrov <baverman@gmail.com>
2017-12-02 00:36:41 +0300

    fix for noexec mounts

--- docker/run-prod.sh
+++ docker/run-prod.sh
@@ -2,7 +2,7 @@
 data=$1
 curdir=$(realpath $(dirname $0))
 src=$(realpath $curdir/..)
-version=$($curdir/gen-ver.sh)
+version=$(sh $curdir/gen-ver.sh)
 uid=$(id -u)
 gid=$(id -g)
 exec docker run -d -v $src:/app -v $data:/data -w /app -u $uid:$gid -e CONFIG=/data/config.py baverman/telenot:$version uwsgi --ini /app/uwsgi.ini
