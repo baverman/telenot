ddc3ae699de77cae5177997acff55ecd96896c97
Anton Bobrov <baverman@gmail.com>
2017-12-01 14:51:25 +0300

    free settings

--- settings/__init__.py
+++ settings/__init__.py
@@ -1,23 +1,35 @@
 import logging
 import os.path
-import importlib
 
 ROOT = os.path.dirname(os.path.dirname(__file__))
 jroot = lambda *parts: os.path.join(ROOT, *parts)
 
+def update_globals(data):
+    globals().update((k, v) for k, v in data.items() if not k.startswith('__'))
+
+# Default settings
 from settings.default import *
 
+# Settings from settings.$PROFILE module
 if 'PROFILE' in os.environ:
+    import importlib
     mod = importlib.import_module('settings.{}'.format(os.environ['PROFILE']))
-    globals().update(vars(mod))
+    update_globals(vars(mod))
 
+# Settings from $CONFIG file
+if 'CONFIG' in os.environ:
+    import runpy
+    ctx = runpy.run_path(os.environ['CONFIG'])
+    update_globals(ctx)
+
+# Local settings
 if os.path.exists(jroot('settings', 'local.py')):
     from .local import *
 
 # check required settings
 invalid_opts = [k for k, v in globals().items() if v == 'UNDEFINED']
 if invalid_opts:
-    raise RuntimeError('Following options must be defined: {}'.format(invalid_opts))
+    raise RuntimeError('Following options must be defined: {}'.format(sorted(invalid_opts)))
 
 if LOGGING:
     logging.dictConfig(LOGGING)
