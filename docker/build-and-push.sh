cb1c8483133d719bd359612df8ebfb83c29bdb5c
Anton Bobrov <baverman@gmail.com>
2017-12-05 16:52:20 +0300

    remove redunant files

--- docker/build-and-push.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-ver=$(./gen-ver.sh)
-docker build -t baverman/telenot:$ver .
-docker push baverman/telenot:$ver
--- docker/gen-ver.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-curdir=$(dirname $0)
-cat $curdir/Dockerfile $curdir/requirements.txt | md5sum | cut -b1-10
